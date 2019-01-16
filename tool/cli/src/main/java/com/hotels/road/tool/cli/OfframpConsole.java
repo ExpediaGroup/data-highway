3cea30e7172ab9d206ace227d73584d8a66d8909
Kyriakos Sideris <k.sideris@gmail.com>
2019-01-16 21:34:05 +0000

    Relocation of cli/tool dev to github

---
+++ tool/cli/src/main/java/com/hotels/road/tool/cli/OfframpConsole.java
@@ -0,0 +1,365 @@
+/**
+ * Copyright (C) 2016-2019 Expedia Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hotels.road.tool.cli;
+
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Properties;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.hotels.road.offramp.client.Committer;
+import com.hotels.road.offramp.client.OfframpClient;
+import com.hotels.road.offramp.client.OfframpOptions;
+import com.hotels.road.offramp.model.DefaultOffset;
+import com.hotels.road.tls.TLSConfig;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.core.OutputStreamAppender;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
+import picocli.CommandLine.Option;
+
+import reactor.core.publisher.Flux;
+
+
+/**
+ * Main class of `data-highway-console-offramp` cli tool.
+ */
+@Command(
+    description = "Data Highway Offramp CLI client",
+    name = "data-highway-console-offramp",
+    sortOptions = false,
+    versionProvider = OfframpConsole.ManifestVersionProvider.class
+)
+public class OfframpConsole implements Callable<Void> {
+
+  private PrintStream msgout;
+  private PrintStream cliout;
+
+  @Option(
+      names = { "-h", "--host"}, required = true,
+      description = "Cluster of Data Highway that Offramp CLI client will connect (required).")
+  private String host;
+
+  @Option(
+      names = { "-u", "--username"}, required = true,
+      description = "User name of the account to consume messages (required).")
+  private String username;
+
+  @Option(
+      names = { "-p", "--password"}, required = true,
+      description = "Password of the provided user (required).")
+  private String password;
+
+  @Option(
+      names = { "-r", "--roadName"}, required = true,
+      description = "Road of which to consume messages (required).")
+  private String roadName;
+
+  @Option(
+      names = { "-s", "--streamName"}, required = true,
+      description = "Stream under which road to consume messages (required).")
+  private String streamName;
+
+  @Option(
+      names = { "-o", "--defaultOffset"},
+      description = "Option between the latest and earliest available message to Offramp service. "
+          + "Enum values: ${COMPLETION-CANDIDATES} (default: ${DEFAULT-VALUE})",
+      completionCandidates = DefaultOffsetCandidates.class)
+  private DefaultOffset defaultOffset = DefaultOffset.LATEST;
+
+  @Option(
+      names = "--initialRequest",
+      description = "Defines how many messages will be requested on the first call to Offramp Server "
+          + "(default: ${DEFAULT-VALUE}).")
+  private Integer initialRequestAmount = 200;
+
+  @Option(
+      names = "--replenishingRequest",
+      description = "Defines how many messages will be requested on subsequent requests. "
+          + "The subsequent requests will only happen after that many messages are consumed downstream "
+          + "(default: ${DEFAULT-VALUE}).")
+  private Integer replenishingRequestAmount = 120;
+
+  @Option(
+      names = "--commitIntervalMs",
+      description = "Interval that messages will be committed in milliseconds "
+          + "(default: ${DEFAULT-VALUE}).")
+  private Long commitIntervalMs = 500L;
+
+  @Option(
+      names = "--numToConsume",
+      description = "Total number of messages to be consumed before termination "
+          + "(default: ${DEFAULT-VALUE}).")
+  private Long numToConsume = Long.MAX_VALUE;
+
+  @Option(
+      names = "--flipOutput",
+      description = "Offramp messages are streamed into stout and CLI prompts into stderr. "
+          + "This option flips the output of messages to stderr and CLI prompts to stout (default: ${DEFAULT-VALUE}).")
+  private boolean flipOutput = false;
+
+  @Option(
+      names = "--tlsTrustAll",
+      description = "Disables certificate checking and hostname verification. "
+          + "This is intended for testing only (default: ${DEFAULT-VALUE}).")
+  private boolean tlsTrustAll = false;
+
+  @Option(
+      names = "--debug",
+      description = "Debug print (default: ${DEFAULT-VALUE}).")
+  private boolean debug = false;
+
+  @Option(names = {"-v", "--version"},
+      versionHelp = true,
+      description = "Print version info and exit")
+  boolean versionRequested;
+
+  @Option(names = "--help",
+      usageHelp = true,
+      description = "Print help info and exit")
+  boolean helpRequested;
+
+
+  public static void main(String[] args) throws Exception {
+    CommandLine.call(new OfframpConsole(), args);
+  }
+
+  @Override
+  public Void call() throws Exception {
+    configureOutput();
+
+    if (debug) {
+      cliout.print(this.getClass() + " was configured with:\n" +
+          "\thost:                      " + this.host                      + "\n" +
+          "\tusername:                  " + this.username                  + "\n" +
+          "\tpassword:                  " +
+          String.join("", Collections.nCopies(this.password.length(), "*"))  + "\n" +
+          "\troadName:                  " + this.roadName                  + "\n" +
+          "\tstreamName:                " + this.streamName                + "\n" +
+          "\tdefaultOffset:             " + this.defaultOffset             + "\n" +
+          "\tinitialRequestAmount:      " + this.initialRequestAmount      + "\n" +
+          "\treplenishingRequestAmount: " + this.replenishingRequestAmount + "\n" +
+          "\tcommitIntervalMs:          " + this.commitIntervalMs          + "\n" +
+          "\tnumToConsume:              " + this.numToConsume              + "\n" +
+          "\ttlsTrustAll:               " + this.tlsTrustAll               + "\n");
+    }
+
+    OfframpOptions<JsonNode> options = getOptions();
+
+    runClient(options);
+
+    return null;
+  }
+
+
+  //
+  // Helper functions
+  //
+
+  /**
+   * Helper function to configure where message and CLI output is directed as well as configure logging
+   */
+  private void configureOutput() {
+    try {
+      if (flipOutput) {
+        msgout = System.err;
+        cliout = System.out;
+      } else {
+        msgout = System.out;
+        cliout = System.err;
+      }
+
+      // retrieve the ch.qos.logback.classic.LoggerContext
+      LoggerContext logCtx = (LoggerContext) org.slf4j.LoggerFactory.getILoggerFactory();
+
+      PatternLayoutEncoder logEncoder = new PatternLayoutEncoder();
+      logEncoder.setContext(logCtx);
+      logEncoder.setPattern("%d{HH:mm:ss.SSS} - %-5level %logger{35} - %msg%n");
+      logEncoder.start();
+
+      OutputStreamAppender logConsoleAppender = new OutputStreamAppender();
+      logConsoleAppender.setContext(logCtx);
+      logConsoleAppender.setOutputStream(cliout);
+      logConsoleAppender.setName("console");
+      logConsoleAppender.setEncoder(logEncoder);
+      logConsoleAppender.start();
+
+      Logger rootLogger = logCtx.getLogger("root");
+      rootLogger.detachAndStopAllAppenders();
+      rootLogger.setLevel(Level.WARN);
+      rootLogger.addAppender(logConsoleAppender);
+
+      Logger roadLogger = logCtx.getLogger("com.hotels.road");
+      roadLogger.setLevel(Level.WARN);
+
+      Logger clientLogger = logCtx.getLogger("com.hotels.road.offramp.client");
+      clientLogger.setLevel(debug ? Level.DEBUG : Level.INFO);
+
+      Logger cliLogger = logCtx.getLogger("com.hotels.road.tool.cli");
+      cliLogger.setLevel(debug ? Level.DEBUG : Level.INFO);
+    } catch (Exception e) {
+      System.err.println("Error configuring the message and cli output:");
+      e.printStackTrace();
+      System.exit(Error.OUTPUT_CONFIGURATION.code);
+    }
+  }
+
+  /**
+   * Helper function to construct OfframpOptions
+   */
+  private OfframpOptions<JsonNode> getOptions() {
+
+    OfframpOptions<JsonNode> options = null;
+
+    try {
+      TLSConfig.Factory tlsFactory = tlsTrustAll ? TLSConfig.trustAllFactory() : null;
+
+      options = OfframpOptions
+          .builder(JsonNode.class)
+          .host(host)
+          .roadName(roadName)
+          .streamName(streamName)
+          .username(username)
+          .password(password)
+          .defaultOffset(defaultOffset)
+          .requestBuffer(initialRequestAmount, replenishingRequestAmount)
+          .tlsConfigFactory(tlsFactory)
+          .build();
+    } catch (Exception e) {
+      cliout.println("Error creating OfframpOptions: ");
+      e.printStackTrace();
+      System.exit(Error.OFFRAMP_OPTIONS_CONFIGURATION.code);
+    }
+
+    return options;
+  }
+
+  /**
+   * Helper function to run OfframpClient
+   */
+  private void runClient(OfframpOptions<JsonNode> options) {
+
+    try (OfframpClient<JsonNode> client = OfframpClient.create(options)) {
+      Committer committer = Committer.create(client, Duration.ofMillis(commitIntervalMs));
+      Flux.from(client.messages())
+          .doOnNext(msgout::println)
+          .doOnError((e) -> cliout.println(e.getMessage()))
+          .doOnNext(committer::commit)
+          .limitRequest(numToConsume)
+          .then()
+          .block();
+    } catch (Exception e) {
+      // handle the exception that has been thrown by OfframpClient.create() OR by close()
+      cliout.println("Error creating or closing Offramp client: ");
+      e.printStackTrace();
+      System.exit(Error.RUN_CLIENT.code);
+    }
+  }
+
+  /**
+   * Class that returns a list of {@link DefaultOffset} enumerations.
+   */
+  private static class DefaultOffsetCandidates extends ArrayList<String> {
+    DefaultOffsetCandidates() {
+      super(
+          Arrays.stream(DefaultOffset.values())
+              .map(DefaultOffset::name)
+              .collect(Collectors.toList())
+      );
+    }
+  }
+
+  /**
+   * {@link IVersionProvider} implementation that returns version information from the jar file's {@code pom.xml} file.
+   */
+  static class ManifestVersionProvider implements IVersionProvider {
+
+    public String[] getVersion() throws Exception {
+      String pomVersion = getVersionFromPom();
+      if (pomVersion != null ) { return new String[] { pomVersion }; }
+
+      String propertiesVersion = getVersionFromProperties();
+      if (propertiesVersion != null ) { return new String[] { propertiesVersion }; }
+
+      String packageVersion = getVersionFromPackage();
+      if (packageVersion != null ) { return new String[] { packageVersion }; }
+
+      return new String[] { "" };
+    }
+
+    private String getVersionFromPom() throws Exception {
+      MavenXpp3Reader reader = new MavenXpp3Reader();
+      Model model = reader.read(
+          new InputStreamReader(
+              OfframpConsole.class
+                  .getResourceAsStream("/META-INF/maven/com.hotels.road/road-tool-cli/pom.xml")
+          )
+      );
+      return model.getVersion();
+    }
+
+    private String getVersionFromProperties() throws Exception {
+      Properties p = new Properties();
+      p.load(
+          OfframpConsole.class
+              .getResourceAsStream("/META-INF/maven/com.hotels.road/road-tool-cli/pom.properties")
+      );
+      return p.getProperty("version", null);
+    }
+
+    private String getVersionFromPackage() throws Exception {
+      Package aPackage = OfframpConsole.class.getPackage();
+      String version = null;
+      if (aPackage != null) {
+        version = aPackage.getImplementationVersion();
+        if (version == null) {
+          version = aPackage.getSpecificationVersion();
+        }
+      }
+      return version;
+    }
+  }
+
+  /**
+   * Enum to list the exit error codes of this CLI tool
+   */
+  enum Error {
+    OUTPUT_CONFIGURATION(1),
+    OFFRAMP_OPTIONS_CONFIGURATION(2),
+    RUN_CLIENT(3);
+
+    private int code;
+
+    Error(int num) {
+      this.code = num;
+    }
+  }
+}
